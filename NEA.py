ae59ea524f7164b2fd123f5b2c6577c02a909028
Charlie Ashdown <charlieashdown15@gmail.com>
2020-01-26 16:10:21 +0000

    Fixed syntax errors and Added Error checking for StartLocation And Endlocation for when the database is fully set up

--- NEA.py
+++ NEA.py
@@ -1,14 +1,32 @@
 #NEA
 #First draft for finding all possible bus routes between any two points
 import time
+def ConnectToDatabase():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="localhost",                    #connects to the database
+        user="root",
+        passwd="LucieLeia0804",
+        database="mysql"
+        )
+    mycursor = mydb.cursor()
+    return(mycursor)
+
 def StartUp():
     Info = []
+    #Stops = []
     with open("data.txt","r") as File:
         for row in File:
             Info.append(row)
+    #myCursor = ConnectToDatabase()
+    #myCursor.execute("SELECT StopName FROM Stop")
+    #for i in range(len(myCursor)):
+        #Stops.append(myCursor[i])
     StartLocation = Info[0] #Get the starting location for the bus
     EndLocation = Info[1] #Get the end location for the bus
     StartTime = Info[2] #Get the wanted arrival time
+    #if StartLocation not in Stops or EndLocation not in Stops:
+        #return("Error")
     if ":" not in StartTime:
         return("Error")
     StartTime = StartTime.split(":")
@@ -23,22 +41,13 @@ def StartUp():
     EndTime.append(StartTime[1])
     EndTime = str(EndTime[0])+ ":" + str(EndTime[1]) 
     return(StartLocation, EndLocation, StartTime, EndTime)
-def ConnectToDatabase():
-    import mysql.connector
-    mydb = mysql.connector.connect(
-        host="localhost",                    #connects to the database
-        user="root",
-        passwd="LucieLeia0804",
-        database="mysql"
-        )
-    mycursor = mydb.cursor()
-    return(mycursor)
-def TimeRange(TimeStart, TimeEnd, StartLocation, ConnectToDataBase):
+
+def TimeRange(TimeStart, TimeEnd, StartLocation):
     Routes = []
-    myCursor = ConnectToDataBase()
+    myCursor = ConnectToDatabase()
     myCursor.execute("SELECT RouteId FROM times WHERE Time > {} AND Time < {} AND StopId = {}").format(TimeStart,TimeEnd,StartLocation)  #searches the databases for all routes leaving the given stop within the time range
     for i in range(len(myCursor)):
-        Routes.append(i)
+        Routes.append(myCursor[i])
     print(Routes)
     return(Routes)
 
@@ -51,7 +60,7 @@ def OneBus(routes, EndLocation, StartLocation, TimeStart, TimeEnd):
         RoutesInTime.append(myCursor) #appends the final product to a list
     return(RoutesInTime) 
 
-def MultipleBusses(routes, TimeRange, OneBus,TimeStart, TimeEnd):
+def MultipleBusses(routes,TimeStart, TimeEnd):
     List = routes
     results = []
     reference = []
@@ -60,7 +69,7 @@ def MultipleBusses(routes, TimeRange, OneBus,TimeStart, TimeEnd):
         myCursor.execute("SELECT StopId FROM times WHERE RouteId = {}").format(List[i])
         StartLocation = myCursor
         routes = TimeRange(TimeStart,TimeEnd,StartLocation)
-        routesinTime= OneBus(routes)
+        routesinTime= OneBus(routes, EndLocation, StartLocation, TimeStart, TimeEnd)
         results.append(routesinTime)
         reference.append(List[i])
     
