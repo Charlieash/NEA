b5473ea655b8f154cc221d1e612727d9fa4a45ed
Charlie Ashdown <charlieashdown15@gmail.com>
2020-01-26 17:18:27 +0000

    Error page php

--- Error.php
+++ Error.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>GET A BUS</title>
+        <title>Error</title>
     </head>
-    <body style="background-color: rgb(69, 42, 69)">
-        <h1 style ="font-size: 900%; text-align: center; "> GET A BUS</h1>
+    <body style="background-color: rgb(69, 42, 69)">You have submitted an incorrect value 
+        <h1 style ="font-size: 900%; text-align: right; "> Error</h1>
+        <form method = "POST">
+            <input type = submit name = return>
+</form>
 </body>
+</html>
+<?php
+if(isset($_POST['return']))
+{header("Location:Neawebpage.php");
+}
