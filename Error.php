9bcc8a566afaa2b4e9f72230d88cf31d37d45c2f
Charlie Ashdown <charlieashdown15@gmail.com>
2020-01-27 11:58:01 +0000

    update error php 2

--- Error.php
+++ Error.php
@@ -4,8 +4,8 @@
         <title>Error</title>
     </head>
     <body style="background-color: rgb(69, 42, 69)">
-        <h1 style ="font-size: 900%; text-align: centre; "> Error</h1>
-        <h2 style = "font-size: 400%; text-align: centre;">You have submitted an incorrect value </h2>
+        <h1 style ="font-size: 900%; text-align: center; "> Error</h1>
+        <h2 style = "font-size: 400%; text-align: center;">You have submitted an incorrect value </h2>
         <form method = "POST">
             <input type = submit name = return>
 </form>
