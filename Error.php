b0ad16657f1a270576b253058a666cc54d3159be
Charlie Ashdown <charlieashdown15@gmail.com>
2020-01-27 12:10:02 +0000

    php error page update 12

--- Error.php
+++ Error.php
@@ -3,11 +3,11 @@
     <head>
         <title>Error</title>
     </head>
-    <body style="background-color: rgb(69, 42, 69)">
+    <body style="background-color: rgb(69, 42, 69); font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">
         <h1 style ="font-size: 900%; text-align: center; "> Error</h1>
         <h2 style = "font-size: 400%; text-align: center;">You have submitted an incorrect value </h2>
         <form method = "POST" style = "text-align:center;">
-            <button class = "btn success" name = return style = "font-size: 220%; font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">Continue </button>
+            <button class = "btn success" name = return style = "font-size: 220%;">Continue </button>
 </form>
 </body>
 </html>
