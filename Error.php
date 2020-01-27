038ae993d2be1633f2d8808a4dec34400e66bf88
Charlie Ashdown <charlieashdown15@gmail.com>
2020-01-27 12:07:08 +0000

    php error page update 9

--- Error.php
+++ Error.php
@@ -6,8 +6,8 @@
     <body style="background-color: rgb(69, 42, 69)">
         <h1 style ="font-size: 900%; text-align: center; "> Error</h1>
         <h2 style = "font-size: 400%; text-align: center;">You have submitted an incorrect value </h2>
-        <form method = "POST" style = "text-align:center; font-size: 600%">
-            <button class = "btn success" name = return >Continue </button>
+        <form method = "POST" style = "text-align:center;">
+            <button class = "btn success" name = return style = "font-size: 600%">Continue </button>
 </form>
 </body>
 </html>
