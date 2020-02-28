12a85558d1d0f43d57730e31fd1193098d70759f
Charlie Ashdown <charlieashdown15@gmail.com>
2020-02-28 11:19:16 +0000

    updated results page

--- results.php
+++ results.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Result</title>
+        <title>GET A BUS</title>
     </head>
     <body style="background-color: rgb(69, 42, 69); font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif">
         <h1 style ="font-size: 500%; text-align: center; "> YOUR POSSIBLE BUSSES</h1>
-        <p style = "font-size: 400%; text-align: center;"><?php $data = strval(file_get_contents('data.txt'));$place = ""; for($x = 0; $x < (strlen($data)); $x++){if($data[$x] != ","){$place = $place.$data[$x];} else{ echo($place."<br>"); $place = "";}} ?>  </p>
+        <p style = "font-size: 400%; text-align: center;"><?php $data = strval(file_get_contents('data.txt'));$place = ""; for($x = 0; $x < (strlen($data)); $x++){if($data[$x] != ","){$place = $place.$data[$x];} else{ echo($place." "."<br>"." "); $place = "";}} ?>  </p>
         <form method = "POST" style = "text-align:center;">
             <button class = "btn success" name = return style = "font-size: 220%;">Continue </button>
 </form>
